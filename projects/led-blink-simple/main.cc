8a6395077024ed465bfe836a9f46aabb7f297fd0
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2016-04-13 22:11:36 -0300

    Update led blink simple example

--- projects/led-blink-simple/main.cc
+++ projects/led-blink-simple/main.cc
@@ -4,18 +4,18 @@ const long int COUNTDOWN = 0xffff;
 
 int main()
 {
-    auto &cpu = ATMega1280::cpu();
-    cpu.cli();
+  auto &cpu = ATMega1280::cpu();
+  cpu.cli();
 
-    cpu.port_b.ddr = 0xff;
-    cpu.port_b.port = 0xff;
+  cpu.port_b.ddr = 0xff;
+  cpu.port_b.port = 0xff;
 
-    long int i = COUNTDOWN;
-    while (1) {
-        while (--i);
-        cpu.port_b.port = ~cpu.port_b.port;
-        i = COUNTDOWN;
-    }
+  long int i = COUNTDOWN;
+  while (1) {
+    while (--i);
+    cpu.port_b.port = ~cpu.port_b.port;
+    i = COUNTDOWN;
+  }
 
-    return 0;
+  return 0;
 }
